7ed369bc5f0e78dc6226c111565f4645dd2f8ca2
shishupal <shishupal.kumar@olacabs.com>
2015-06-16 07:50:27 +0530

    bootstrap method added for angular for dynamic template app

--- static/js/test/451.js
+++ static/js/test/451.js
@@ -2,31 +2,62 @@ var App = angular.module("AngularApp", []), JS;
 App.run(function($rootScope) {
 	$rootScope.name = "AppParent";
 });
-App.controller("AppController", ['$scope', function($scope) {
+App.controller("AppController", ['$scope', '$rootScope',function($scope, $rootScope) {
 	$scope.added = false;
+	$scope.html = "";
 	$scope.addNewController = function(e){
 		if(!$scope.added){
-			JS();
+			JS($scope);
+			$rootScope.$broadcast("add");
 			console.log("New Controller added.");
 			$scope.added = true;
 		}else{
 			console.log("New Controller already added.");
 		}
 	};
-}]);
-App.controller("AppControllerV2", ['$scope', function($scope) {
-	$scope.checkNewController = function(e){
-		console.log("Dynamic controller working.");
+	$scope.checkNewController = function(){
+		console.log("New Controller working.");
 	};
 }]);
-JS = function(){
-	var html = '<div class="second-controller ng-scope" ng-controller="AppControllerV2">'
-				+'<div><button ng-click="checkNewController(this)">Click me to confirm.</button></div>'
-			+'</div>';
-	$("div.container").append(html);
-	App.controller("AppControllerV2", ['$scope', function($scope) {
-		$scope.checkNewController = function(e){
-			console.log("Dynamic controller working.");
+// App.directive("appDirective", function(){
+// 	return {
+// 		restrict: 'E',
+// 		templateUrl: 'templates/angularjs/451_directive.html'
+// 	};
+// });
+App.directive("appDirective", function(){
+	return {
+		restrict: 'E',
+		controller: function($scope){
+			$scope.$on("add", function(){
+				$scope.status = true;
+			});
+		},
+		template: '<div ng-show="status"><button ng-click="checkNewController(this)">Click me to confirm.</button></div>'
+	};
+});
+// App.controller("AppControllerV2", ['$scope', function($scope) {
+// 	$scope.checkNewController = function(e){
+// 		console.log("Dynamic controller working.");
+// 	};
+// }]);
+JS = function(scope){
+	App.directive("appDirective", function(){
+		return {
+			restrict: 'E',
+			template: '<div><button ng-click="checkNewController(this)">Click me to confirm.</button></div>'
 		};
-	}]);
+	});
+	// var html = '<div class="second-controller">'
+	// 			+'<div><button ng-click="checkNewController(this)">Click me to confirm.</button></div>'
+	// 			+'<div><button>Click me to confirm.</button></div>'
+	// 		+'</div>';
+	// scope.html = html;
+
+	// $("div.container").append(html);
+	// App.controller("AppControllerV2", ['$scope', function($scope) {
+	// 	$scope.checkNewController = function(e){
+	// 		console.log("Dynamic controller working.");
+	// 	};
+	// }]);
 };
---
+++ static/js/test/452.js
@@ -0,0 +1,27 @@
+var App = null;
+function startApp(btn){
+	if(App && App.name){
+		console.log("App alredy bootstraped.");
+		return;
+	}
+	var html = '<div class="container" ng-app="AngularApp"><div class="js-controller" ng-controller="AppController"><div><div><button ng-click="checkNewController()">NG added</button></div></div></div></div>';
+	btn.parents(".js-controller").append(html);
+	App = angular.module("AngularApp", []);
+	App.run(function($rootScope) {
+		$rootScope.name = "AppParent";
+	});
+	App.controller("AppController", ['$scope', '$rootScope',function($scope, $rootScope) {
+		$scope.added = false;
+		$scope.html = "";
+		$scope.checkNewController = function(){
+			console.log("New Controller working.");
+		};
+	}]);
+	angular.bootstrap(btn.parents(".js-controller"), ['AngularApp']);
+}
+$(document).on("ready", function() {
+	$("#parent_btn").on("click", function(e){
+		var btn = $(e.currentTarget);
+		startApp(btn);
+	});
+});
