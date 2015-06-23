b5b3e48b0240834eb38dcfa7ff114ef27337469b
shishupal <shishupal.kumar@olacabs.com>
2015-06-23 14:32:22 +0530

    added directive in multiple view

--- app/multiple_view_js.js
+++ app/multiple_view_js.js
@@ -1,17 +1,28 @@
+"use strict";
 (function(){
 	angular.module('APP.controllers', []).
-	controller('MultipleViewController', function($scope, $location) {
-		$scope.email = "user@domain.com";
-		$scope.name = "UserName";
+	controller('EventController', function($scope, $location) {
 		$scope.renderView = function(name){
 			$location.path(name);
 		};
+	}).
+	controller('MultipleViewController', ['$scope',  function($scope) {
+		$scope.email = "user@domain.com";
+		$scope.name = "UserName";
+	}]);
+	angular.module('APP.directives', []).directive('appVersion', function() {
+	  return {
+	    template: '<div>Version : {{name}}-v1.0</div>'
+	  };
 	});
-	angular.module('APP', ['APP.controllers','ngRoute']).
+	angular.module('APP', ['APP.controllers','APP.directives','ngRoute']).
     config(['$routeProvider', function($routeProvider) {
 	    $routeProvider.
 	    	when("/email", {templateUrl: "/templates/angularjs/partials/multiple_view_email.html", controller: "MultipleViewController"}).
 	    	when("/name", {templateUrl: "/templates/angularjs/partials/multiple_view_name.html", controller: "MultipleViewController"});
 //	    	otherwise({redirectTo: '/name'});
     }]);
+	angular.module('APP').run(function($rootScope) {
+		$rootScope.name = "AngularApp";
+	});
 })();
