c98c19f2169ea2181a73cbf707d2cc7a3962714e
shishupal <shishupal.kumar@olacabs.com>
2015-07-18 01:44:50 +0530

    login app created

---
+++ php/application/views/app/loginV3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Angular app | Login v3</title>
+	<link rel="stylesheet" href="/static/libs/bootstrap/bootstrap-v3.1.1/css/bootstrap.css" />
+<style type="text/css">
+.error{
+	color : #f00;
+}
+</style>
+</head>
+<body>
+	<div><center>Welcome!</center></div>
+	<div ng-app="App">
+		<div ng-view></div>
+	</div>
+	<script type="text/javascript" src="/static/libs/angular/angular_1.3.14.js"></script>
+	<script type="text/javascript" src="/static/libs/angular/angular-route_1.4.2.js"></script>
+	<script src="/app/loginAppV3/app.js"></script>
+</body>
+</html>
