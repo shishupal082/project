7b428e5fc24b076b1318918950c7d2b9188f74d2
shishupal <shishupal.kumar@olacabs.com>
2015-12-10 14:44:11 +0530

    handle php variables from angular

---
+++ app/dashboard/home_app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+(function () {
+    var app = angular.module('app', []);
+    app.run(['$rootScope', function ($rootScope) {
+        $rootScope.name = "AngularApp";
+    }]);
+}());
+
+(function(){
+    var injectParams = ['$scope', '$rootScope'];
+    var HomeController = function ($scope, $rootScope) {
+        $scope.load_count = 101;
+        $scope.getLoadCountStyle = function(){
+            return "display:block";
+        };
+    };
+    HomeController.$inject = injectParams;
+    angular.module('app').controller('HomeController', HomeController).directive('pvtLinks', function() {
+      return {
+        templateUrl: '/pvt/links/pvt_link.html'
+      };
+    }).directive('tsLinks', function() {
+      return {
+        templateUrl: '/pvt/links/ts_link.html'
+      };
+    }).directive('gssLinks', function() {
+      return {
+        templateUrl: '/pvt/links/gss_link.html'
+      };
+    });
+}());
