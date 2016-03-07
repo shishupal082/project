8c301820239f63f59b25b0718f6afd9715d1f215
shishupal <shishupal.kumar@olacabs.com>
2016-03-07 21:59:04 +0530

    use sl4j logging mechanism

--- DW/src/main/java/com/test/filter/RequestFilter.java
+++ DW/src/main/java/com/test/filter/RequestFilter.java
@@ -1,32 +1,26 @@
 package com.test.filter;
 
-import com.test.utils.Utils;
+import org.slf4j.MDC;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by shishupal.kumar on 03/02/16.
  */
-@Getter
-@Setter
+@Slf4j
 public class RequestFilter implements ContainerRequestFilter {
-	private String requestId;
-	public RequestFilter(){
-		requestId = Utils.getRandomNumber();
-	}
-
-	public void logInfo(String string){
-		System.out.println(requestId + " : " + string);
-	}
+	public RequestFilter(){}
 
 	@Override
 	public void filter(final ContainerRequestContext requestContext) throws IOException {
-		System.out.println("Request filter start");
+		MDC.remove("X-Transaction-Key");
+		MDC.put("X-Transaction-Key", UUID.randomUUID().toString());
+		log.error("Request filter start");
 	}
 }
