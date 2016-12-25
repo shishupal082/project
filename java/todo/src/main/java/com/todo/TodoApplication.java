ae2077fc689dcddf2a4290bf5fbf51373e839c1a
shishupal kumar <shishupal082@gmail.com>
2016-12-25 13:38:24 +0530

    todo application folder created

---
+++ java/todo/src/main/java/com/todo/TodoApplication.java
@@ -0,0 +1,10 @@
+package com.todo;
+
+/**
+ * Created by shishupalkumar on 25/12/16.
+ */
+public class TodoApplication {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World");
+    }
+}
