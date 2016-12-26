e28682e5ad7c65ce41477ac986e7ab8d70e5e336
shishupal kumar <shishupal082@gmail.com>
2016-12-26 09:05:16 +0530

    log4j basic setup done

--- java/todo/src/main/java/com/todo/TodoApplication.java
+++ java/todo/src/main/java/com/todo/TodoApplication.java
@@ -1,10 +1,16 @@
 package com.todo;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Created by shishupalkumar on 25/12/16.
  */
+
 public class TodoApplication {
+    private static Logger logger = LoggerFactory.getLogger(TodoApplication.class);;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        PropertyConfigurator.configure("log4j.properties");
+        logger.info("Hello World");
     }
 }
