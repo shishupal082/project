19afeaa2e29e64d28e7ebc79c7d236ade35b4491
shishupal kumar <shishupal082@gmail.com>
2016-12-30 18:32:35 +0530

    java example files added

---
+++ java/example/src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shishupalkumar on 30/12/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
