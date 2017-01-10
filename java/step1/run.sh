5f87dbf84618b463042a6cfb66b2a96a816232a4
shishupal kumar <shishupal082@gmail.com>
2017-01-10 20:50:38 +0530

    TodoApp started working

--- java/step1/run.sh
+++ java/step1/run.sh
@@ -1,7 +1,7 @@
 mvn clean install
 echo $(date +"%I:%M:%S") : "Adding dependencies"
-cp ../../../../program_files/jars/step2-1.0-SNAPSHOT.jar target/
-cp ../../../../program_files/jars/java-calculator-1.0.0.rc16.jar target/
+cp $HOME/program_files/jars/step2-1.0-SNAPSHOT.jar target/
+cp $HOME/program_files/jars/java-calculator-1.0.0.rc16.jar target/
 echo $(date +"%I:%M:%S") : "Dependencies added"
 echo $(date +"%I:%M:%S") : "Running programms"
 echo "-----------------------"
