6ce96c9fdf3a20a66897447c719b7ca9727071c4
shishupal kumar <shishupal082@gmail.com>
2017-01-11 19:16:37 +0530

    added simplified pom.xml file in step1

--- java/step1/run.sh
+++
@@ -1,8 +0,0 @@
-mvn clean install
-echo $(date +"%I:%M:%S") : "Adding dependencies"
-cp $HOME/program_files/jars/step2-1.0-SNAPSHOT.jar target/
-cp $HOME/program_files/jars/java-calculator-1.0.0.rc16.jar target/
-echo $(date +"%I:%M:%S") : "Dependencies added"
-echo $(date +"%I:%M:%S") : "Running programms"
-echo "-----------------------"
-java -jar target/step1-*.jar
