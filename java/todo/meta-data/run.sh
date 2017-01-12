9dbb926b55ebafca421c93eab5026c3505739bf9
shishupal kumar <shishupal082@gmail.com>
2017-01-12 16:31:33 +0530

    added bash tag in run shell script

--- java/todo/meta-data/run.sh
+++ java/todo/meta-data/run.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 java -jar todo-*-SNAPSHOT.jar server meta-data/env_config.yml
--- java/todo/run.sh
+++ java/todo/run.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 java -jar target/todo-*-SNAPSHOT.jar server meta-data/env_config.yml
