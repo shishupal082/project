f80dbd46e03dd6b4c482d344f490e0e3ee46b590
shishupal kumar <shishupal082@gmail.com>
2017-02-09 13:51:22 +0530

    added multiple support for capitalization server and client application

--- java/example/src/socketClient/CapitalizationClient.java
+++ java/example/src/socketClient/CapitalizationClient.java
@@ -1,21 +1,94 @@
 package socketClient;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 /**
  * Created by shishupalkumar on 12/08/16.
  */
 public class CapitalizationClient {
     private BufferedReader systemIn;
+    private String protocol = "byte";
+    private Socket socket;
     private CapitalizationClient() {
         systemIn = new BufferedReader(new InputStreamReader(System.in));
+    }
+    private String getResponse() throws IOException {
+        String response;
+        if (protocol.equals("byte")) {
+            InputStream inFromServer = socket.getInputStream();
+            BufferedInputStream buf = new BufferedInputStream(inFromServer);
+            DataInputStream dataInputStream = new DataInputStream(buf);
+            String parsedResponse = "";
+            int dataIn = dataInputStream.readByte();
+            while(dataIn > 0) {
+                parsedResponse += (char)dataIn;
+                StringTokenizer st = new StringTokenizer(parsedResponse, "|");
+                Boolean isResponseEnd = false;
+                while (st.hasMoreElements()) {
+                    if (st.nextElement().equals("END")) {
+                        isResponseEnd = true;
+                        while (dataInputStream.available() > 0) {
+                            dataInputStream.readByte();
+                        }
+                        break;
+                    }
+                }
+                if (isResponseEnd) {
+                    break;
+                }
+                dataIn = dataInputStream.read();
+            }
+            response = parsedResponse;
+        } else {
+            BufferedReader socketIn = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+            response = socketIn.readLine();
+        }
+        System.out.println("Response : " + response);
+        return response;
+    }
+    private void sendRequest(String request) throws IOException {
+        System.out.println("Request : " + request);
+        if (protocol.equals("byte")) {
+            OutputStream outToServer = socket.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(outToServer);
+            request += "|END";
+            dataOutputStream.writeBytes(request);
+        } else {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(request);
+        }
+    }
+    private void startCommunication() throws IOException{
+        System.out.println(getResponse());
+        System.out.print("Enter string 1 : ");
+        String userInput = systemIn.readLine();
+        sendRequest(userInput);
+        System.out.println("Socket output : " + getResponse());
+
+        System.out.print("Enter string 2 : ");
+        userInput = systemIn.readLine();
+        sendRequest(userInput);
+        System.out.println("Socket output : " + getResponse());
+
+        sendRequest("Dummy Input String.");
+        System.out.println("Socket output : " + getResponse());
+
+        System.out.print("Enter string 3 : ");
+        userInput = systemIn.readLine();
+        sendRequest(userInput);
+        System.out.println("Socket output : " + getResponse());
     }
     private void connectToServer(String[] args) throws IOException {
         String ip = "127.0.0.1";
@@ -24,29 +97,11 @@ public class CapitalizationClient {
             ip = args[0];
             port = Integer.parseInt(args[1]);
         }
-        Socket socket = new Socket(ip, port);
-        PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        //read Socket welcome message.
-        System.out.println(socketIn.readLine());
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(ip, port), 10000);
 
-        System.out.print("Enter string 1 : ");
-        String userInput = systemIn.readLine();
-        socketOut.println(userInput);
-        System.out.println("Socket output : " + socketIn.readLine());
-
-        System.out.print("Enter string 2 : ");
-        userInput = systemIn.readLine();
-        socketOut.println(userInput);
-        System.out.println("Socket output : " + socketIn.readLine());
-
-        socketOut.println("Dummy Input String.");
-        System.out.println("Socket output : " + socketIn.readLine());
-
-        System.out.print("Enter string 3 : ");
-        userInput = systemIn.readLine();
-        socketOut.println(userInput);
-        System.out.println("Socket output : " + socketIn.readLine());
+        this.startCommunication();
+        socket.close();
     }
     /**
      * Runs the client application.
--- java/example/src/socketServer/CapitalizationServer.java
+++ java/example/src/socketServer/CapitalizationServer.java
@@ -1,27 +1,33 @@
 package socketServer;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 /**
  * Created by shishupalkumar on 12/08/16.
  */
 public class CapitalizationServer {
-
     /**
      * Application method to run the server runs in an infinite loop
-     * listening on port 9898.  When a connection is requested, it
+     * listening on port ****.  When a connection is requested, it
      * spawns a new thread to do the servicing and immediately returns
      * to listening.  The server keeps a unique client number for each
      * client that connects just to show interesting logging
      * messages.  It is certainly not necessary to do this.
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("The capitalization server is running.");
+        String protocol = "byte";
+        System.out.println("The capitalization server is running with protocol : " + protocol);
         int clientNumber = 0;
         Integer port = 8080;
         if (args.length >= 1) {
@@ -30,7 +36,7 @@ public class CapitalizationServer {
         ServerSocket listener = new ServerSocket(port);
         try {
             while (true) {
-                new Capitalizer(listener.accept(), clientNumber++).start();
+                new Capitalizer(listener.accept(), clientNumber++, protocol).start();
             }
         } finally {
             listener.close();
@@ -39,19 +45,66 @@ public class CapitalizationServer {
 
     /**
      * A private thread to handle capitalization requests on a particular
-     * socket.  The client terminates the dialogue by sending a single line
-     * containing only a period.
+     * socket.  The client can communicate as long as they want.
      */
     private static class Capitalizer extends Thread {
         private Socket socket;
         private int clientNumber;
+        private String protocol;
 
-        public Capitalizer(Socket socket, int clientNumber) {
+        public Capitalizer(Socket socket, int clientNumber, String protocol) throws IOException {
             this.socket = socket;
             this.clientNumber = clientNumber;
+            this.protocol = protocol;
             System.out.println("New connection with client# " + clientNumber + " at " + socket);
         }
-
+        private String getRequest() throws IOException {
+            String request;
+            if (protocol.equals("byte")) {
+                InputStream inFromServer = socket.getInputStream();
+                BufferedInputStream inBuf = new BufferedInputStream(inFromServer);
+                DataInputStream dataInputStream = new DataInputStream(inBuf);
+                String parsedRequest = "";
+                int dataIn = dataInputStream.readByte();
+                while(dataIn > 0) {
+                    parsedRequest += (char) dataIn;
+                    StringTokenizer st = new StringTokenizer(parsedRequest, "|");
+                    Boolean isRequestEnd = false;
+                    while (st.hasMoreElements()) {
+                        if (st.nextElement().equals("END")) {
+                            isRequestEnd = true;
+                            while (dataInputStream.available() > 0) {
+                                dataInputStream.readByte();
+                            }
+                            break;
+                        }
+                    }
+                    if (isRequestEnd) {
+                        break;
+                    }
+                    dataIn = dataInputStream.read();
+                }
+                request = parsedRequest;
+            } else {
+                BufferedReader socketIn = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+                request = socketIn.readLine();
+            }
+            System.out.println(clientNumber + " : Request : " + request);
+            return request;
+        }
+        private void sendResponse(String response) throws IOException {
+            System.out.println(clientNumber + " : Response : " + response);
+            if (protocol.equals("byte")) {
+                response += "|END";
+                OutputStream outToServer = socket.getOutputStream();
+                DataOutputStream dataOutputStream = new DataOutputStream(outToServer);
+                dataOutputStream.writeBytes(response);
+            } else {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(response);
+            }
+        }
         /**
          * Services this thread's client by first sending the
          * client a welcome message then repeatedly reading strings
@@ -59,29 +112,16 @@ public class CapitalizationServer {
          */
         public void run() {
             try {
-
-                // Decorate the streams so we can send characters
-                // and not just bytes.  Ensure output is flushed
-                // after every newline.
-                BufferedReader socketIn = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-
-                // Send a welcome message to the client.
-                out.println("Hello, you are client #" + clientNumber + ".");
-
-                // Get messages from the client, line by line; return them
-                // capitalized
+                this.sendResponse("Hello, you are client #" + clientNumber + ".");
                 while (true) {
-                    String input = socketIn.readLine();
-                    System.out.println(clientNumber + ": input : " + input);
+                    String input = this.getRequest();
                     if (input == null || input.equals(".")) {
-                        System.out.println(clientNumber + ": socketClosed");
-                        out.println("socketClosed");
+                        System.out.println(clientNumber + " : socketClosed");
+                        this.sendResponse("socketClosed");
                         break;
                     }
-                    System.out.println(clientNumber + ": Capitalizing... : " + input);
-                    out.println(input.toUpperCase());
+                    System.out.println(clientNumber + " : Capitalizing... : " + input);
+                    this.sendResponse(input.toUpperCase());
                 }
             } catch (IOException e) {
                 System.out.println("Error handling client# " + clientNumber + ": " + e);
