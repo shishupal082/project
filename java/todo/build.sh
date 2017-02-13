030cc522fb683a72022ad1915a4686ee4b2a9436
shishupal kumar <shishupal082@gmail.com>
2017-02-13 12:55:29 +0530

    do correct order build

--- java/todo/build.sh
+++ java/todo/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo *****Start building application*****
-rm meta-data/todo-*.jar
 mvn clean install
+rm meta-data/todo-*.jar
 cp target/todo-*.jar meta-data/
 rm -rf target
 echo *****Build complete*****
