f87e9a5b1740345aa87e248c3edb47c493e79dab
shishupal kumar <shishupal082@gmail.com>
2017-11-10 06:57:59 +0530

    app config created work as standlone application

--- java/todo/run.bat
+++ java/todo/run.bat
@@ -1,1 +1,1 @@
-java -jar meta-data/todo-1.4-bita-SNAPSHOT.jar server meta-data/config/env_config.yml meta-data/config/app_config_2.yaml
+java -jar meta-data/todo-1.4-bita-SNAPSHOT.jar server meta-data/config/env_config.yml meta-data/config/app_config.yaml
