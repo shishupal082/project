1a9483735b24414e06e1cd18d40c4560f5cb55e6
shishupal <shishupal@shishupals-MacBook-Pro.local>
2018-04-16 18:36:46 +0530

    add uploaded_files folder to git and change folder name for copy build project

--- java/todo/build.sh
+++ java/todo/build.sh
@@ -8,14 +8,14 @@ rm -rf target
 
 echo [Info] Copying file to desired location
 
-rm -rf ../../todo
-mkdir ../../todo
-mkdir ../../todo/meta-data
-mkdir ../../todo/meta-data/config
-cp readme.txt ../../todo/
-cp meta-data/todo-*.jar ../../todo/meta-data/
-cp meta-data/config/env_config.yml ../../todo/meta-data/config/
-cp meta-data/config/yaml_object.yaml ../../todo/meta-data/config/
-cp meta-data/config/available_resources.yaml ../../todo/meta-data/config/
+rm -rf ../../todo-app
+mkdir ../../todo-app
+mkdir ../../todo-app/meta-data
+mkdir ../../todo-app/meta-data/config
+cp readme.txt ../../todo-app/
+cp meta-data/todo-*.jar ../../todo-app/meta-data/
+cp meta-data/config/env_config.yml ../../todo-app/meta-data/config/
+cp meta-data/config/yaml_object.yaml ../../todo-app/meta-data/config/
+cp meta-data/config/available_resources.yaml ../../todo-app/meta-data/config/
 
 echo *****Build complete*****
