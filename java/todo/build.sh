37ebf55d6951a6eb81f975e0f3dc4c00b94ba687
shishupal <shishupal@shishupals-MacBook-Pro.local>
2019-11-05 19:21:49 +0530

    rename compiled folder name

--- java/todo/build.sh
+++ java/todo/build.sh
@@ -6,17 +6,17 @@ rm meta-data/todo-*.jar
 cp target/todo-*.jar meta-data/
 rm -rf target
 
-echo [Info] Copying file to desired location
+echo [INFO] Copying file to desired location
 
-rm -rf ../../todo-app
-mkdir ../../todo-app
-mkdir ../../todo-app/meta-data
-mkdir ../../todo-app/meta-data/config
-cp readme.txt ../../todo-app/
-cp meta-data/todo-*.jar ../../todo-app/meta-data/
-cp meta-data/config/env_config.yml ../../todo-app/meta-data/config/
-cp meta-data/config/env_config_custom_logging.yaml ../../todo-app/meta-data/config/
-cp meta-data/config/yaml_object.yaml ../../todo-app/meta-data/config/
-cp meta-data/config/available_resources.yaml ../../todo-app/meta-data/config/
+rm -rf ../../todo
+mkdir ../../todo
+mkdir ../../todo/meta-data
+mkdir ../../todo/meta-data/config
+cp readme.txt ../../todo/
+cp meta-data/todo-*.jar ../../todo/meta-data/
+cp meta-data/config/env_config.yml ../../todo/meta-data/config/
+cp meta-data/config/env_config_custom_logging.yaml ../../todo/meta-data/config/
+cp meta-data/config/yaml_object.yaml ../../todo/meta-data/config/
+cp meta-data/config/available_resources.yaml ../../todo/meta-data/config/
 
 echo *****Build complete*****
