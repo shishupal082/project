a345e2e0508888d1d556782b42be94b87302bd7f
shishupal <shishupal@shishupals-MacBook-Pro.local>
2019-11-29 20:26:26 +0530

    added log for php file upload

--- php/application/controllers/upload.php
+++ php/application/controllers/upload.php
@@ -12,19 +12,33 @@ class Upload extends CI_Controller {
         $relativePath = "pvt/uploaded_files/";
         $config['upload_path']   = DOCUMENT_ROOT."/".$relativePath;
         $config['allowed_types'] = 'gif|jpg|png|jpeg|pdf|iso|zip|rar';
-        $config['max_size']      = 10000;
-        $config['max_width']     = 10240;
-        $config['max_height']    = 7680;
+        $config['max_size']      = 0; //In kb, 0 for no limit
+        $config['max_width']     = 10240; // In pixel
+        $config['max_height']    = 7680; // In pixel
+
+        if (isset($_FILES['userfile'])) {
+            log_message("INFO", "File upload start: ".json_encode($_FILES['userfile']));
+        } else {
+            log_message("INFO", "Invalid file upload attribute.");
+            log_message("INFO", "Redirecting to --> ".base_url());
+            redirect(base_url());
+            return 0;
+        }
         $this->load->library('upload', $config);
         if (!$this->upload->do_upload('userfile')) {
+            log_message("INFO", "Unable to upload file.");
             $error = array('error' => $this->upload->display_errors());
             echo json_encode($error, TRUE);
         }
         else {
+            log_message("INFO", "File upload complete.");
             $data = array('upload_data' => $this->upload->data());
             if (isset($data["upload_data"]["orig_name"])) {
-                redirect(base_url().$relativePath.$data["upload_data"]["orig_name"]);
+                $redirectPath = base_url().$relativePath.$data["upload_data"]["orig_name"];
+                log_message("INFO", "Redirecting to --> ".$redirectPath);
+                redirect($redirectPath);
             } else {
+                log_message("INFO", json_encode($data));
                 echo json_encode($data);
             }
         }
