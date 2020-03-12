539a086482042acbb1e17b790bef9827a74d6f4b
shishupal <shishupal@shishupals-MacBook-Pro.local>
2020-03-12 19:43:06 +0530

    move Domino from yardModel to stack

--- static/js/stack.js
+++ static/js/stack.js
@@ -569,7 +569,105 @@ var BT = (function(){
     };
     return BT;
 })();
-
+var Domino = (function() {
+    var maxRow = 3, maxCol = 5;
+    function isValidIndex(r, c) {
+        if (typeof r == "number" && !isNaN(r)) {
+            if (typeof c == "number" && !isNaN(c)) {
+                if (r >=0 && r<maxRow && c>=0 && c<maxCol) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    function isValidDominoData(d) {
+        if (ValidateDomino == false) {
+            return true;
+        }
+        var validRowCount = 0;
+        var validColCount = 0, validColCountStatus = false;
+        var isRowIncremented = false;
+        for (var i = 0; i < d.data.length; i++) {
+            validColCount = 0;
+            isRowIncremented = false;
+            for (var j = 0; j < d.data[i].length; j++) {
+                if (d.data[i][j] !== null) {
+                    validColCount++;
+                    if (isRowIncremented == false) {
+                        validRowCount++;
+                        isRowIncremented = true;
+                    }
+                }
+                if (validColCount == 5) {
+                    validColCountStatus = true;
+                }
+            }
+        }
+        if (validRowCount == maxRow && validColCountStatus) {
+            return true;
+        }
+        return false;
+    };
+    function Domino(name) {
+        var data = [];
+        for (var i = 0; i < maxRow; i++) {
+            data.push([]);
+            for (var j = 0; j < maxCol; j++) {
+                data[i].push(null);
+            }
+        }
+        this.name = name;
+        this.data = data;
+        this.isValidData = isValidDominoData(this);
+        return this;
+    }
+    Domino.prototype.setData = function(r, c, data) {
+        if (isValidIndex(r,c)) {
+            if (this.data[r][c] !== null) {
+                var logText = "Data already present.";
+                throw logText;
+            }
+            this.data[r][c] = data;
+        } else {
+            var logText = "Invalid index: r=" + r+", c="+c;
+            console.log("Domino name: " + this.name);
+            throw logText;
+        }
+        this.isValidData = isValidDominoData(this);
+        return true;
+    };
+    Domino.prototype.setRowData = function(r, data) {
+        if (typeof data == "object" && !isNaN(data.length)) {
+            for (var i = 0; i < data.length; i++) {
+                this.setData(r, i, data[i]);
+            }
+        }
+        return true;
+    };
+    Domino.prototype.getData = function() {
+        var response = [];
+        for (var i = 0; i < this.data.length; i++) {
+            response.push([]);
+            for (var j = 0; j < this.data[i].length; j++) {
+                if (this.data[i][j] !== null) {
+                    response[i].push(this.data[i][j]);
+                }
+            }
+        }
+        if (this.isValidData) {
+            return response;
+        }
+        console.log("Domino name: " + this.name);
+        console.log(this.data);
+        var logText = "Invalid Domino Data.";
+        throw logText;
+    };
+    Domino.prototype.isValidDomino = function() {
+        return this.isValidData;
+    };
+    return Domino;
+})();
 var Stack = function(selector, context) {
     return new Stack.fn.init(selector, context);
 };
@@ -770,6 +868,9 @@ Stack.extend({
     },
     getTable: function(tableContent, tableId) {
         return new Table(tableContent, tableId);
+    },
+    getDomino: function(dominoName) {
+        return new Domino(dominoName);
     },
     log: function(log) {
         Logger.log(log);
--- static/js/yard/yardModel.js
+++ static/js/yard/yardModel.js
@@ -5,116 +5,6 @@
 */
 (function($S) {
 var ValidateDomino = false
-var Domino = (function() {
-    var maxRow = 3, maxCol = 5;
-    function isValidIndex(r, c) {
-        if (typeof r == "number" && !isNaN(r)) {
-            if (typeof c == "number" && !isNaN(c)) {
-                if (r >=0 && r<maxRow && c>=0 && c<maxCol) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-    function isValidDominoData(d) {
-        if (ValidateDomino == false) {
-            return true;
-        }
-        var validRowCount = 0;
-        var validColCount = 0, validColCountStatus = false;
-        var isRowIncremented = false;
-        for (var i = 0; i < d.data.length; i++) {
-            validColCount = 0;
-            isRowIncremented = false;
-            for (var j = 0; j < d.data[i].length; j++) {
-                if (d.data[i][j] !== null) {
-                    validColCount++;
-                    if (isRowIncremented == false) {
-                        validRowCount++;
-                        isRowIncremented = true;
-                    }
-                }
-                if (validColCount == 5) {
-                    validColCountStatus = true;
-                }
-            }
-        }
-        if (validRowCount == maxRow && validColCountStatus) {
-            return true;
-        }
-        return false;
-    };
-    function Domino(name) {
-        var data = [];
-        for (var i = 0; i < maxRow; i++) {
-            data.push([]);
-            for (var j = 0; j < maxCol; j++) {
-                data[i].push(null);
-            }
-        }
-        this.name = name;
-        this.data = data;
-        this.isValidData = isValidDominoData(this);
-        return this;
-    }
-    Domino.prototype.setData = function(r, c, data) {
-        if (isValidIndex(r,c)) {
-            if (this.data[r][c] !== null) {
-                var logText = "Data already present.";
-                throw logText;
-            }
-            this.data[r][c] = data;
-        } else {
-            var logText = "Invalid index: r=" + r+", c="+c;
-            console.log("Domino name: " + this.name);
-            throw logText;
-        }
-        this.isValidData = isValidDominoData(this);
-        return true;
-    };
-    Domino.prototype.setRowData = function(r, data) {
-        if (typeof data == "object" && !isNaN(data.length)) {
-            for (var i = 0; i < data.length; i++) {
-                this.setData(r, i, data[i]);
-            }
-        }
-        return true;
-    };
-    Domino.prototype.getData = function() {
-        var response = [];
-        for (var i = 0; i < this.data.length; i++) {
-            response.push([]);
-            for (var j = 0; j < this.data[i].length; j++) {
-                if (this.data[i][j] !== null) {
-                    response[i].push(this.data[i][j]);
-                }
-            }
-        }
-        if (this.isValidData) {
-            return response;
-        }
-        console.log("Domino name: " + this.name);
-        console.log(this.data);
-        var logText = "Invalid Domino Data.";
-        throw logText;
-    };
-    Domino.prototype.isValidDomino = function() {
-        return this.isValidData;
-    };
-    Domino.prototype.enableValidate = function() {
-        ValidateDomino = true;
-        return ValidateDomino;
-    };
-    Domino.prototype.disableValidate = function() {
-        ValidateDomino = false;
-        return ValidateDomino;
-    };
-    Domino.prototype.getValidateStatus = function() {
-        return ValidateDomino;
-    };
-    return Domino;
-})();
 var YardModel = function(selector, context) {
     return new YardModel.fn.init(selector, context);
 };
@@ -136,24 +26,25 @@ lsKey = keyMapping[lsKey];
 
 var LS = $S.getLocalStorage();
 
+function getTableHtml(yardData, name) {
+    var tableData = [];
+    if (yardData && yardData[name]) {
+        tableData = yardData[name];
+    }
+    if (ValidateDomino) {
+        /* Checking data intigrity.*/
+        var d = $S.getDomino(name);
+        for (var i = 0; i < tableData.length; i++) {
+            d.setRowData(i, tableData[i]);
+        }
+        tableData = d.getData();
+        /* Checking data intigrity End.*/
+    }
+    var table = $S.getTable(tableData, name);
+    return table.getHtml();
+}
+
 YardModel.extend({
-    getTableHtml: function(yardData, name) {
-        var tableData = [];
-        if (yardData && yardData[name]) {
-            tableData = yardData[name];
-        }
-        if (ValidateDomino) {
-            /* Checking data intigrity.*/
-            var d = new Domino(name);
-            for (var i = 0; i < tableData.length; i++) {
-                d.setRowData(i, tableData[i]);
-            }
-            tableData = d.getData();
-            /* Checking data intigrity End.*/
-        }
-        var table = $S.getTable(tableData, name);
-        return table.getHtml();
-    },
     getYardTableContent: function(yardComponent, requiredContent) {
         var tableContent = [];
         for (var i = 0; i < requiredContent.length; i++) {
@@ -170,13 +61,11 @@ YardModel.extend({
         return tableContent;
     },
     enableDomino: function () {
-        var domino = new Domino();
-        domino.enableValidate();
+        ValidateDomino = true;
         return true;
     },
     disableDomino: function () {
-        var domino = new Domino();
-        domino.disableValidate();
+        ValidateDomino = false;
         return false;
     },
     getDisplayYardDominoBoundary: function() {
