02c2c224f6237dc1d08b4537021e4e50a12d94fd
shishupal <shishupal@shishupals-MacBook-Pro.local>
2020-04-25 13:45:28 +0530

    simple nodejs application completed

---
+++ nodejs/index.js
@@ -0,0 +1,113 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const AppConstant = require("./static/AppConstant.js");
+const $S = require("../static/js/stack.js");
+
+const hostname = "localhost";
+const port = 3000;
+
+
+function loadFavicon(req, res) {
+    res.statusCode = 200;
+    res.setHeader(AppConstant.CONTENT_TYPE, AppConstant.IMAGE_X_ICON);
+    var filePath = path.resolve("./../favicon.ico");
+    fs.createReadStream(filePath).pipe(res);
+}
+
+var availableResources = {
+    "/": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "index.html"
+    },
+    "/index.html": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "index.html"
+    },
+    "/index.html/": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "index.html"
+    },
+    "/indexData.json": {
+        responseType: AppConstant.APPLICATION_JSON,
+        filePath: "./indexData.json"
+    },
+    "/app/yard1": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "../app/yard1/index.html"
+    },
+    "/app/yard1/": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "../app/yard1/index.html"
+    },
+    "/app/yard1/index.html": {
+        responseType: AppConstant.TEXT_HTML,
+        filePath: "../app/yard1/index.html"
+    }
+};
+
+var cssPaths = [];
+cssPaths.push("/static/libs/bootstrap-v3.1.1.css");
+cssPaths.push("/app/yard1/static/css/style.css");
+
+var jsPaths = [];
+jsPaths.push("/static/js/stack.js");
+jsPaths.push("/static/js/model.js");
+jsPaths.push("/static/js/yard/yardApiModel.js");
+jsPaths.push("/static/libs/jquery-2.1.3.js");
+jsPaths.push("/app/yard1/static/js/yard1ComponentModel.js");
+jsPaths.push("/app/yard1/static/js/yard1PointModel.js");
+jsPaths.push("/app/yard1/static/js/yard1Controller.js");
+jsPaths.push("/app/yard1/static/js/yard1Script.js");
+
+var jsonPaths = [];
+jsonPaths.push("/app/yard1/static/json/yard.json");
+
+var requestMapper = {};
+Object.assign(requestMapper, availableResources);
+
+for (var i = 0; i < cssPaths.length; i++) {
+    requestMapper[cssPaths[i]] = {
+        responseType: AppConstant.TEXT_CSS,
+        filePath: "../" + cssPaths[i]
+    };
+}
+for (var i = 0; i < jsPaths.length; i++) {
+    requestMapper[jsPaths[i]] = {
+        responseType: AppConstant.TEXT_JS,
+        filePath: "../" + jsPaths[i]
+    };
+}
+for (var i = 0; i < jsonPaths.length; i++) {
+    requestMapper[jsonPaths[i]] = {
+        responseType: AppConstant.APPLICATION_JSON,
+        filePath: "../" + jsonPaths[i]
+    };
+}
+
+const server = http.createServer((req, res) => {
+    // console.log(req.headers);
+    if (req.url == "/favicon.ico") {
+        loadFavicon(req, res);
+    } else {
+        $S.log("Request: " + req.url + ", " + req.method);
+        var hrefPath = $S.getUrlAttribute(req.url, "hrefPath");
+        if (requestMapper[hrefPath]) {
+            $S.log("Precessing request.");
+            var mappedData = requestMapper[hrefPath];
+            res.statusCode = 200;
+            res.setHeader(AppConstant.CONTENT_TYPE, mappedData.responseType);
+            fs.createReadStream(mappedData.filePath).pipe(res);
+        } else {
+            res.statusCode = 200;
+            res.setHeader(AppConstant.CONTENT_TYPE, AppConstant.TEXT_HTML);
+            res.end('<center>Invalid url.</center>');
+        }
+        $S.log("Request end.");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`*** server running at http://${hostname}:${port} ***`);
+});
+
---
+++ nodejs/readme.txt
@@ -0,0 +1,144 @@
+Nodejs works on module
+Reactjs works on component
+
+Nodejs          Reactjs
+--------------------------
+module          component
+require         imports
+exports
+__filename
+__dirname
+
+
+Uses:
+    const square = require("square.js")
+
+Module in nodejs is same as window in javascript (i.e. global variable)
+
+Download and installation
+-------------------------
+If node is not installed already then download and install from
+
+https://nodejs.org/en/
+
+This package will install:
+    Node.js v12.16.2 to /usr/local/bin/node
+    npm v6.14.4 to /usr/local/bin/npm
+
+Make sure that /usr/local/bin is in your $PATH
+
+>> node (It will take you to node REPL)
+>> node -v
+>> npm -v (npm is a part of node)
+
+node REPL (Read-Eval-Print-Loop)
+    For exit press CMD+C twice
+    It is same as developer console in chrome
+
+Install dependencies from package.json
+>> npm install
+
+Project setup
+-------------
+>> npm init
+It will create package.json
+
+If package.json already exist then type
+>> type NUL > index.js
+It will create empty index.js file
+
+
+Running of project
+------------------
+>> node index     or,
+>> node index.js
+
+It will execute program written in index.js
+
+
+Node.js Modules & its types
+----------------------------
+
+1) File-based Modules
+2) Core Modules
+3) Enternal Node Modules
+
+1) File-based Modules
+*******************
+module.exports & require
+
+square.js
+----
+// exports.area = (a) => (a*a);
+// exports.perimeter = (a) => (4*a);
+
+var Area = (a) => (a*a);
+var Perimeter = (a) => (4*a);
+
+module.exports.area = Area;
+module.exports.perimeter = Perimeter;
+
+index.js
+---
+const square = require("./square.js")
+
+console.log(__filename);
+console.log(__dirname);
+
+
+console.log("Hello World!");
+console.log(square.area(5));
+console.log(square.perimeter(5));
+
+
+2) Core Modules
+*************
+Parts of core modules
+
+path, fs, os, util, ...
+
+path
+--
+const path = require('path');
+
+const filename = path.join(__filename);
+const basename = path.basename(filename);
+const extname = path.extname(filename);
+
+console.log(filename);
+console.log(basename);
+console.log(extname);
+
+http
+--
+
+
+3) Enternal Node Modules
+**********************
+Third party modules
+Installed using npm
+node_modules folder in your Node application
+
+
+
+Now starting of applications
+-----------------------------
+
+Add following in package.json
+
+"scripts": {
+    "start": "node index"
+},
+
+Dependencies
+------------
+http, path, fs
+
+
+To start server
+---------------
+>> npm start
+
+It should start working
+
+
