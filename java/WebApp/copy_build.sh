4abced17a0d07ff0cf396a235c8c9f33309b7f16
shishupal <shishupal@shishupals-MacBook-Pro.local>
2020-04-29 17:26:01 +0530

    minor fix

--- java/WebApp/copy_build.sh
+++ java/WebApp/copy_build.sh
@@ -6,10 +6,10 @@ rm -rf ../../web-app/*
 mkdir ../../web-app/meta-data
 
 cp readme.txt ../../web-app/
-cp run.bat ../../web-app/
+cp run_app.bat ../../web-app/
 
 cp meta-data/favicon.ico ../../web-app/meta-data/
-cp meta-data/env_config.yml ../../web-app/meta-data/
+cp meta-data/app_env_config.yml ../../web-app/meta-data/
 cp meta-data/WebApp-*.jar ../../web-app/meta-data/
 
 echo [INFO] Copy file complete
---
+++ java/WebApp/run_app.bat
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+java -jar meta-data/WebApp-1.0.1-SNAPSHOT.jar meta-data/app_env_config.yml
