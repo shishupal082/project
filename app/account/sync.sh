aa1112d1f312930b0177da0985ff46398c781a4b
shishupal <shishupal@shishupals-MacBook-Pro.local>
2020-06-07 10:22:49 +0530

    added config change

--- app/account/sync.sh
+++ app/account/sync.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+
 cp index.html currentbal/
 cp index.html currentbalbydate/
 cp index.html journal/
+cp index.html trialbalance/
 cp index.html journalbydate/
 cp index.html ledger/
 cp index.html summary/
+cp index.html summarybydate/
+
