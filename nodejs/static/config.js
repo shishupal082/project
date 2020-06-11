8b47cc3f73d6b479a6f05678d37c2bd8c7d519a2
shishupal <shishupal082@gmail.com>
2020-06-11 14:37:55 +0530

    nodejs setup completed for windows

--- nodejs/static/config.js
+++ nodejs/static/config.js
@@ -2,7 +2,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-    hostname: process.env.HOSTNAME,
+    environment: process.env.NODE_ENV,
+    hostname: "localhost",//process.env.HOSTNAME,
     port: process.env.PORT,
     endpoint: process.env.API_URL,
     masterKey: process.env.API_KEY
