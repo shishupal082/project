6a28c2987ad3f8ecb85b6096d693743f28a849b8
shishupal <shishupal082@gmail.com>
2020-06-25 16:48:31 +0530

    added 404 page for codeignitor php app

--- php/application/controllers/error.php
+++ php/application/controllers/error.php
@@ -1,13 +1,13 @@
 <?php
 class Error extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-		$this->_name = "Error";
-	}
-	public function index(){
-		$this->show_404();
-	}
-	public function show_404() {
-		echo "404";
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->_name = "Error";
+    }
+    public function index(){
+        $this->show_404();
+    }
+    public function show_404() {
+        $this->load->view("page_404");
+    }
 }
---
+++ php/application/views/page_404.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html ng-app="app">
+<head>
+	<meta charset="utf-8">
+	<base href="<?php echo base_url();?>">
+	<meta name="environment" content="<?php echo ENVIRONMENT; ?>">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>404 Page not found</title>
+<script>
+<?php echo $this->load->view("analytics_tracking_php.dev.php", true);?>
+</script>
+<style type="text/css">
+* {
+	font-family: sans-serif;
+}
+</style>
+</head>
+<body>
+	<center>
+		<div><h1>PHP Apps</h1></div>
+		<div><a href="/">404 Page not found</a></div>
+	</center>
+</body>
+</html>
