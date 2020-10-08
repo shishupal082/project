d464f0a0ffc2d1a8cdc6fec05b491ae1167aa4c2
shishupal <shishupal082@gmail.com>
2020-10-08 06:09:40 +0530

    build reactapp for monitor

--- php/application/views/app/monitor.php
+++ php/application/views/app/monitor.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8" />
     <title>Monitoring Application</title>
-    <link rel="stylesheet" type="text/css" href="/static/libs/bootstrap-v4.4.1.css">
-    <link rel="stylesheet" type="text/css" href="/app/monitoring/static/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/static/libs/bootstrap-v4.4.1.css"/>
+    <link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/app/monitoring/static/css/style.css"/>
 
 <?php echo $this->load->view("analytics_tracking_php.dev.php", true);?>
 
