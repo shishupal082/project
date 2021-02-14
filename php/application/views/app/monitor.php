5024f5496289da0e9bf3ff1e7828c1790b47d5a5
shishupal <shishupal082@gmail.com>
2021-02-14 07:35:10 +0530

    syncing both file

--- php/application/views/app/monitor.php
+++ php/application/views/app/monitor.php
@@ -6,16 +6,21 @@
     <link rel="stylesheet" type="text/css" href="/static/libs/bootstrap-v4.4.1.css"/>
     <link rel="stylesheet" type="text/css" href="/static/css/style.css"/>
     <link rel="stylesheet" type="text/css" href="/app/account/static/css/react-app.css"/>
-    <link rel="stylesheet" type="text/css" href="/app/monitor/static/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/app/monitor/static/css/monitor-style.css"/>
 
 <?php echo $this->load->view("analytics_tracking_php.dev.php", true);?>
 
 </head>
 <body class="theme-grey">
     <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div class="d-none" style="display: none;">
+      <input type="hidden" style="display: none;" name="uploadFileInstruction" id="uploadFileInstruction"
+        value="(Supported type: pdf,jpeg,jpg,png,csv and txt, max size < 10MB)"/>
+      <input type="hidden" style="display: none;" name="uploadTextInstruction" id="uploadTextInstruction" value=""/>
+    </div>
     <div id="root"><center>Loading...</center></div>
+<script type="text/javascript" src="/static/libs/jquery-2.1.3.js"></script>
 <script type="text/javascript">
-
 var GLOBAL = {
     baseapi: "",
     appControlDataPath: "/app/monitor/static/data/appControlData_",
@@ -25,8 +30,22 @@ var GLOBAL = {
 
 GLOBAL.appControlApi = ["/app/monitor/static/data/appControlData.json"];
 
+
+GLOBAL.addTextFilenamePattern = "YYYY/-/MM/-/DD/-/hh/-/mm/-device-report.csv";
+GLOBAL.headingPattern = "device";
+
+GLOBAL.validTeamAppControl = ["appControlDataTeam01",
+              "appControlDataTeam02","appControlDataTeam03",
+              "appControlDataTeam04","appControlDataTeam05","appControlDataTeam06"];
+
+
+GLOBAL.uploadFileInstruction = document.getElementById("uploadFileInstruction").value;
+GLOBAL.uploadTextInstruction = document.getElementById("uploadTextInstruction").value;
+
+
 GLOBAL.gtag = gtag;
 GLOBAL.appVersion = "1.0.1";
+GLOBAL.JQ = $;
 window.GLOBAL = GLOBAL;
 
 </script>
