905f20920d2d850698feac78ac6e345811ed4623
shishupal <shishupal082@gmail.com>
2021-03-12 10:11:02 +0530

    monitor app activated for js

--- reactjs/reactapp/copy_build.sh
+++ reactjs/reactapp/copy_build.sh
@@ -26,8 +26,8 @@ distDir=dist-data-entry-app
 distDir=dist-assets-app
 distDir=dist-ml2-app
 distDir=dist-monitoring-app
-distDir=dist-monitoringv2-app
 distDir=dist-data-display-app
+distDir=dist-monitoringv2-app
 
 addLog "Dist directory : ${distDir}"
 
