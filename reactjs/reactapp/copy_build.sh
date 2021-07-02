b05f5e8756dd4e61e918a1f8eaecbb235f5d8849
shishupal <shishupal082@gmail.com>
2021-07-02 10:49:09 +0530

    improve attendance app

--- reactjs/reactapp/copy_build.sh
+++ reactjs/reactapp/copy_build.sh
@@ -25,11 +25,11 @@ distDir=dist-account-app
 distDir=dist-data-entry-app
 distDir=dist-assets-app
 distDir=dist-ml2-app
-distDir=dist-data-display-app
 distDir=dist-monitoring-app
+distDir=dist-data-display-app
 distDir=dist-ftp-app
-distDir=dist-attendance-app
 distDir=dist-project-tracking-app
+distDir=dist-attendance-app
 
 addLog "Dist directory : ${distDir}"
 
