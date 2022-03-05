d832f485dc3d9a5e9201512ff1b900c2f00aa48a
shishupal <shishupal082@gmail.com>
2022-03-05 18:13:21 +0530

    nodejs version tracking improved

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -1,9 +1,9 @@
 oldVersion=1.0.1
 newVersion=1.0.1
-echo "$(date +"%Y-%m-%d %T") v=$newVersion" >> src/version.txt
+echo "$(date +"%Y-%m-%d %T") v=$newVersion" >> version.txt
 sed -i "s/$oldVersion/$newVersion/1" src/common/AppConstant.js
 rm -rf build/*.tar.gz
 npm install
-tar czf build/nodejs-project-v$newVersion.tar.gz node_modules src config package.json run_tcp_server.sh run_ping_thread.sh run_db_test.sh
+tar czf build/nodejs-project-v$newVersion.tar.gz node_modules src config version.txt package.json run_tcp_server.sh run_ping_thread.sh run_db_test.sh
 cp build/nodejs-project-v$newVersion.tar.gz /f/app-nodejs/
 
